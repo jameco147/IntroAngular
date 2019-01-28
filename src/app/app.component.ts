9d28b94d454009f4c3c494ddde4fbb8c6cf0a237
Javi <bonjavi89@gmail.com>
2019-01-28 23:20:47 +0100

    Subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'estudio';
+  butonText: string = 'Save';
+  isDisabled:boolean = false;
+  name:string = '';
+  position:string = '';
+
+  saveEvent(){
+    if (this.butonText === 'Save') {
+      this.butonText = 'Edit';
+      this.isDisabled = true;
+    } else {
+      this.butonText = 'Save';
+      this.isDisabled = false;
+      console.log(this.butonText);
+    }
+  }
+  
+  showInformation(){
+    alert(this.name + " " + this.position);  
+  }
+
+  resetInformation(){
+    this.name='';
+    this.position='';
+  }
+
+
 }
